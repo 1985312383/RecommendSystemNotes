c878525797115f2fe0bee0bbe2f2c0bb1d1ada64
柯慕灵 <1985312383@qq.com>
2024-02-08 02:04:32 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,5 +5,24 @@ import { defineUserConfig } from 'vuepress'
 export default defineUserConfig({
   bundler: viteBundler(),
   theme: defaultTheme(),
+  base: '/RecommendSystemNotes/',
+  themeConfig: {
+    // 添加导航栏
+    nav: [
+      { text: 'vue', link: '/' },
+      { text: 'css', link: '/blog/' },
+      { text: 'js', link: '/zhihu/' },
+      {
+        text: 'github',
+        // 这里是下拉列表展现形式。
+        items: [
+          { text: 'focus-outside', link: 'https://github.com/txs1992/focus-outside' },
+          { text: 'stylus-converter', link: 'https://github.com/txs1992/stylus-converter' }
+        ]
+      }
+    ],
+    // 为以下路由添加侧边栏
+    sidebar: ['/', '/git', '/vue']
+  },
 })
 
